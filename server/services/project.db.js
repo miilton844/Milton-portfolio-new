84f9486ee2e30c272abf1ed6b580028911f05079
unknown <oso.lindo@gmail.com>
2021-10-29 19:15:48 +0300

    cleanup code

--- server/services/project.db.js
+++ server/services/project.db.js
@@ -1,17 +1,15 @@
-const { db ,admin } = require('../firebase_init/firebase_init')
+const { db } = require('../firebase_init/firebase_init')
 
 
 const getProjects = async () => {
   const path = 'projects';
   let projects = [];
   const snapshot = await db.collection(path).get();
+  console.log(snapshot)
   snapshot.forEach((doc) => {
-    console.log(doc.id, "=>", doc.data());
     projects.push(doc.data())
     projects[projects.length - 1].id = parseInt(doc.id)
-
   });
-  console.log(projects)
   return projects;
 }
 
